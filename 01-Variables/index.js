e2f35de72141f78881d05fda102be427b37646db
Pru <david_2428107@hotmail.com>
2024-11-22 12:06:02 -0600

    guardar todo

---
+++ 01-Variables/index.js
@@ -0,0 +1,26 @@
+    let Raul //comun
+    const Jesus = 5 // no puede cambiar su valor
+    var Edson // la variable puede cambiar en cualquier momento
+
+// String normal
+    let Apellido = "Sanchez"
+// Tipo de vairable constannte 
+    let Edad = 20
+// untipo de variable String template 
+    let Nombre = `Hector`;
+// este es un arreglo
+    let Nombres = ["alfredo","jesus","jose"]
+// este tipo de cosas serian objetos
+    let Comidas = 
+    {
+        "enchiladas":20, 
+        "totopos":30,
+        "arroz":10
+    }
+
+// tipo de dato booleanos
+    EsMayor = true
+    EsMenor = false
+
+
+    console.log (Nombre)
---
+++ 04 - Objetos/index.js
@@ -0,0 +1,28 @@
+// crear mi objeto
+const Deportes = {
+"futbol":20,
+"Basquetbol":30,
+"voleybol":40,
+"tenis":{
+    "rojo":51,
+    "azul":52
+    },
+"americano":60
+}
+
+// una variable para obtener el valor dentro de una anidacion para botener el valor del dicho equipo en este caso
+const tenis = Deportes['tenis']['azul'];
+
+console.log(Deportes)
+console.log(tenis)
+//muestra atributos dentro de la primera llave impresion
+console.log(Object.keys(Deportes))
+//muestra los valores de la primera llave en una sola impresion
+console.log(Object.values(Deportes))
+
+// este for me muestra los valores por separado
+for (const key in Deportes) {
+    const element = Deportes[key];
+    console.log(element)
+}
+
