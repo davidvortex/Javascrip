0bbe99b93b284f915ea12b727f7e2c511d8f1c09
davidvortex <david@hotmail.com>
2024-11-26 11:39:13 -0600

    terminado

--- problemas/ejercicio03.js
+++ problemas/ejercicio03.js
@@ -1,1 +1,12 @@
 // progama una función que dada un string te devuelva un array de textos separados 
+
+// Función que recibe un string y lo divide en un array de textos separados por espacios
+function separarTexto(inputString) {
+    // Se divide el string por espacios y se devuelve como un array
+    return inputString.split(' ');
+}
+
+// Ejemplo de uso
+let texto = "Hola, este es un ejemplo de texto";
+let resultado = separarTexto(texto);
+console.log(resultado);
